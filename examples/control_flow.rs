15dcd872fc5c424895efd61cf1b833dd62ff2034
kgv <kgv@users.noreply.github.com>
2020-04-01 21:24:24 +0300

    Add examples

---
+++ examples/control_flow.rs
@@ -0,0 +1,25 @@
+#![feature(proc_macro_hygiene)]
+
+use core::fmt::{Debug, Formatter, Result};
+use format::lazy_format;
+
+struct Foo(usize);
+
+impl Debug for Foo {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        let alternate = f.alternate();
+        let bar = lazy_format!(|f| if alternate {
+            write!(f, "{:#x}", self.0)
+        } else {
+            write!(f, "{:x}", self.0)
+        });
+        f.debug_tuple("Foo")
+            .field(&format_args!("{}", bar))
+            .finish()
+    }
+}
+
+fn main() {
+    assert_eq!("Foo(75bcd15)", format!("{:?}", Foo(0123456789)));
+    assert_eq!("Foo(\n    0x75bcd15,\n)", format!("{:#?}", Foo(0123456789)));
+}
---
+++ examples/nested_debug_help_struct.rs
@@ -0,0 +1,29 @@
+#![feature(proc_macro_hygiene)]
+
+use core::fmt::{Debug, Formatter, Result};
+use format::lazy_format;
+
+struct Foo {
+    bar: [u32; 10],
+}
+
+impl Debug for Foo {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        let bar = lazy_format!(|f| f.debug_list().entries(&self.bar).finish());
+        f.debug_struct("Foo")
+            .field("bar", &format_args!("{}", bar))
+            .finish()
+    }
+}
+
+fn main() {
+    assert_eq!(
+        "Foo { bar: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] }",
+        format!(
+            "{:?}",
+            Foo {
+                bar: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+            }
+        )
+    );
+}
